b5ddc8ddbff24430dd5ca8fde4090a7bedbe47f7
KenanMuradov <maljerov.1999@gmail.com>
2023-02-10 16:49:00 +0300

    Search Update

--- DapperTask/MainWindow.xaml.cs
+++ DapperTask/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -18,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Z.Dapper.Plus;
 
 namespace DapperTask;
 
@@ -30,9 +32,9 @@ public partial class MainWindow : Window,INotifyPropertyChanged
 
     private void OnPropertyChanged([CallerMemberName] String propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-    private List<Product> _products;
+    private List<Product>? _products;
 
-    public List<Product> Products
+    public List<Product>? Products
     {
         get => _products;
         set
@@ -76,6 +78,15 @@ IF EXISTS(SELECT * FROM sys.databases WHERE NAME = 'OnlineStore')
 SELECT @isDatabaseExist";
 
         IsDatabaseExist = await _connection.ExecuteScalarAsync<bool>(checkDbCommand);
+
+        if(IsDatabaseExist)
+        {
+            var startIndex = conStr.IndexOf(';') + 1;
+
+            _connection.ConnectionString = conStr.Insert(startIndex, "Database = OnlineStore;");
+
+            IsDatabaseExist = true;
+        }
     }
 
     private async void btnDataBase_Click(object sender, RoutedEventArgs e)
@@ -95,32 +106,36 @@ IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Produc
         Price money NOT NULL,
         Count int NOT NULL DEFAULT(0)
     );
+    IF EXISTS(SELECT * FROM sys.databases WHERE NAME = 'Northwind')
+    BEGIN
+        INSERT INTO OnlineStore.dbo.Products(Name,Price,Count)
+        SELECT [ProductName] AS Name
+        ,[UnitPrice] AS Price
+        ,[UnitsInStock] AS Count
+        FROM [Northwind].[dbo].[Products]
+    END
     END
 END";
 
-
-        var getDataCommand = @"IF EXISTS(SELECT * FROM sys.databases WHERE NAME = 'Northwind')
-BEGIN
-    INSERT INTO OnlineStore.dbo.Products(Name,Price,Count)
-    SELECT [ProductName] AS Name
-    ,[UnitPrice] AS Price
-    ,[UnitsInStock] AS Count
-    FROM [Northwind].[dbo].[Products]
-END";
         await _connection.ExecuteAsync(databaseCreateCommand);
         await _connection.ExecuteAsync(tableCreateCommand);
-        await _connection.ExecuteAsync(getDataCommand);
+
+        var conStr = _connection.ConnectionString;
+
+        var startIndex = conStr.IndexOf(';') +1;
+
+        _connection.ConnectionString = conStr.Insert(startIndex,"Database = OnlineStore;");
 
         IsDatabaseExist = true;
     }
 
     private async void btnRefresh_Click(object sender, RoutedEventArgs e)
     {
-        var getDataCommand = "SELECT * FROM OnlineStore.dbo.Products";
+        var getDataCommand = "SELECT * FROM Products";
 
         var collection = await _connection.QueryAsync<Product>(getDataCommand);
 
-        DataList.ItemsSource= collection.ToList();
+        Products= collection.ToList();
     }
 
     private async void btnSearch_Click(object sender, RoutedEventArgs e)
@@ -128,10 +143,27 @@ END";
         if(string.IsNullOrWhiteSpace(SearchTxt.Text)) 
             return;
 
-        var getDataCommand = "SELECT * FROM OnlineStore.dbo.Products WHERE Name LIKE '%@SearchText%'";
+        var getDataCommand = $"SELECT * FROM OnlineStore.dbo.Products WHERE Name LIKE '%{SearchTxt.Text}%'";
 
-        var collection = await _connection.QueryAsync<Product>(getDataCommand, new {SearchText = SearchTxt.Text });
+        var collection = await _connection.QueryAsync<Product>(getDataCommand);
 
-        DataList.ItemsSource = collection.ToList();
+        Products = collection.ToList();
+    }
+
+    private void btnDelete_Click(object sender, RoutedEventArgs e)
+    {
+        DapperPlusManager.Entity<Product>().Table("Products");
+
+        var collection = DataList.SelectedItems.Cast<Product>().ToList();
+
+        if (collection.Count == 0 || Products is null)
+            return;
+
+        var startIndex = Products.IndexOf(collection[0]);
+
+        foreach (var item in collection)
+            Products.Remove(item);
+
+        _connection.BulkDelete(collection);
     }
 }
